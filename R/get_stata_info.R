a8455ca55941b07321b5174d72b7c62e97a0f18b
Joonas Miettinen <joonas.miettinen@cancer.fi>
2020-09-14 13:25:43 +0300

    get_stata_info refactoring
    used nordcan_survival_settings here as well.

--- R/get_stata_info.R
+++ R/get_stata_info.R
@@ -1,4 +1,6 @@
-get_stata_info <- function(stata = NULL) {
+
+
+get_stata_info <- function(stata_exe_path, work_dir) {
   ## make template for Stata commad file
   dofile_template <-
     "
@@ -17,50 +19,37 @@ get_stata_info <- function(stata = NULL) {
     "
 
   ## Check STATA/files exist or not;
-  path_stata <- paste0(system.file(package = "nordcansurvival"), "/stata/path_stata.RData")
 
-  if (is.null(stata)) {
-    if (file.exists(path_stata)) {
-      load(path_stata)
-    } else {
-      stop("User must give the path of STATA")
-    }
-  } else {
-    if (!file.exists(stata)) {
-      stop(sprintf("Can not find Stata software: %s!", stata))
-    } else {
-      # save the path of stata to installed package, so  user don't need to specify it everytime.
-      save(stata, file = path_stata)
-    }
-  }
+  settings <- nordcan_survival_settings(
+    stata_exe_path = stata_exe_path, work_dir = work_dir
+  )
+  ado_dir <- settings[["ado_dir"]]
 
-
-  wd <- getwd()
-  dir_ado <- paste0(system.file(package = "nordcansurvival"), "/stata/ado")
-
-  dofile <- sprintf( dofile_template, wd, dir_ado, dir_ado, dir_ado, dir_ado)
-
-  dofile_name <-  paste0(wd, "/get_stata_info.do")
+  dofile <- sprintf(
+    dofile_template, settings[["work_dir"]], ado_dir, ado_dir, ado_dir, ado_dir
+  )
+  
+  dofile_name <-  paste0(settings[["work_dir"]], "/get_stata_info.do")
   cat(dofile, file = dofile_name )
 
   ## comand line to run STATA on Windows or Linux OS;
   flag <- ifelse(.Platform$OS.type[1] == "windows", "/e", "-b")
-  CMD <- sprintf("%s %s %s", stata, flag , dofile_name)
+  CMD <- sprintf("%s %s %s", stata_exe_path, flag , dofile_name)
 
   ## Run command
   system(CMD, wait = TRUE)
 
   ## Find the output, and print it to console.
-  tmp_info <- paste0(wd, "/get_stata_info.log")
+  info_path <- paste0(settings[["work_dir"]], "/get_stata_info.log")
   for (i in 1 :100) {
-    if (file.exists(tmp_info)) {
-      tmp_info_R <- readLines(tmp_info)
-      print(tmp_info_R)
-      break
+    if (file.exists(info_path)) {
+      stata_info <- readLines(info_path)
+      break()
     }
     Sys.sleep(0.5)
   }
 
+  return(stata_info)
 }
 
 
